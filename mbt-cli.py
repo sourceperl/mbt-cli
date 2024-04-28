606f4bc5c104bdbaffe883dc98ecb79102aef034
sourceperl <3809271+sourceperl@users.noreply.github.com>
2024-04-28 16:08:16 +0200

    update mbt-cli.py

--- mbt-cli.py
+++ mbt-cli.py
@@ -6,7 +6,6 @@ A modbus/TCP command-line cli.
 """
 
 import argparse
-from dataclasses import dataclass
 import cmd
 from pyModbusTCP.client import ModbusClient
 
@@ -16,7 +15,6 @@ NAME = 'mbt-cli'
 VERSION = '0.0.1'
 
 
-@dataclass
 class RequestData:
     address: int = 0
     number: int = 0
@@ -31,81 +29,12 @@ class MbtCli(cmd.Cmd):
 
     @property
     def prompt(self):
-        """Set cli prompt (like "127.0.0.1:8428> ")"""
+        """Set cli prompt (like "127.0.0.1:502> ")"""
         return f'{mbus_cli.host}:{mbus_cli.port}> '
 
     def emptyline(self) -> bool:
         """Avoid empty line execute again the last command"""
         return False
-
-    def do_debug(self, arg: str = ''):
-        """Check or set debug status"""
-        # try to set
-        debug_set = arg.strip().lower()
-        if debug_set:
-            if debug_set == 'on':
-                mbus_cli.debug = True
-            elif debug_set == 'off':
-                mbus_cli.debug = False
-            else:
-                print('unable to set debug flag')
-        # show status
-        debug_str = 'on' if mbus_cli.debug else 'off'
-        print(f'debug is {debug_str}')
-
-    def do_host(self, arg: str = ''):
-        """Check or set host"""
-        # try to set
-        host_set = arg.strip().lower()
-        if host_set:
-            try:
-                mbus_cli.host = str(host_set)
-            except ValueError:
-                print('unable to set host')
-        # show status
-        print(f'current host is "{mbus_cli.host}"')
-
-    def do_port(self, arg: str = ''):
-        """Check or set port"""
-        # try to set
-        port_set = arg.strip().lower()
-        if port_set:
-            try:
-                mbus_cli.port = int(port_set)
-            except ValueError:
-                print('unable to set port')
-        # show status
-        print(f'current port value is {mbus_cli.port}')
-
-    def do_timeout(self, arg: str = ''):
-        """Check or set timeout"""
-        # try to set
-        timeout_set = arg.strip().lower()
-        if timeout_set:
-            try:
-                mbus_cli.timeout = float(timeout_set)
-            except ValueError:
-                print('unable to set timeout')
-        # show status
-        print(f'timeout is {mbus_cli.timeout} s')
-
-    def do_read_coils(self, arg: str = ''):
-        """Read coils (function 1)"""
-        # process command args: "read coils [address] [number]"
-        try:
-            self._parse_read_args(arg)
-            self.request.read_list = mbus_cli.read_coils(self.request.address, self.request.number)
-            self._dump_results()
-        except ValueError as e:
-            print(e)
-
-    def do_version(self, _arg):
-        """Print version"""
-        print(f'{NAME} {VERSION}')
-
-    def do_exit(self, _arg):
-        """Exit from cli"""
-        return True
 
     def _parse_read_args(self, arg: str):
         args_l = arg.split()
@@ -121,16 +50,108 @@ class MbtCli(cmd.Cmd):
     def _dump_results(self):
         if self.request.read_list:
             for idx in range(0, len(self.request.read_list)):
-                print(f'{idx} 0x{self.request.address + idx:04x} ({self.request.address + idx:d}) {self.request.read_list[idx]}')
+                msg = f'{idx} 0x{self.request.address + idx:04x} ' \
+                      f'({self.request.address + idx:d}) {self.request.read_list[idx]}'
+                print(msg)
+
+    def do_debug(self, arg: str = ''):
+        """Check or set debug status\n\ndebug [on/off]"""
+        # try to set
+        debug_set = arg.strip().lower()
+        if debug_set:
+            if debug_set == 'on':
+                mbus_cli.debug = True
+            elif debug_set == 'off':
+                mbus_cli.debug = False
+            else:
+                print('unable to set debug flag')
+        # show status
+        debug_str = 'on' if mbus_cli.debug else 'off'
+        print(f'debug is {debug_str}')
+
+    def do_host(self, arg: str = ''):
+        """Check or set host\n\nhost [hostname/ip address/fqdn]"""
+        # try to set
+        host_set = arg.strip().lower()
+        if host_set:
+            try:
+                mbus_cli.host = str(host_set)
+            except ValueError:
+                print('unable to set host')
+        # show status
+        print(f'current host is "{mbus_cli.host}"')
+
+    def do_port(self, arg: str = ''):
+        """Check or set port\n\nport [tcp port]"""
+        # try to set
+        port_set = arg.strip().lower()
+        if port_set:
+            try:
+                mbus_cli.port = int(port_set)
+            except ValueError:
+                print('unable to set port')
+        # show status
+        print(f'current port value is {mbus_cli.port}')
+
+    def do_timeout(self, arg: str = ''):
+        """Check or set timeout\n\ntimeout [timeout value in s]"""
+        # try to set
+        timeout_set = arg.strip().lower()
+        if timeout_set:
+            try:
+                mbus_cli.timeout = float(timeout_set)
+            except ValueError:
+                print('unable to set timeout')
+        # show status
+        print(f'timeout is {mbus_cli.timeout} s')
+
+    def do_unit_id(self, arg: str = ''):
+        """Check or set unit-id\n\nunit_id [unit_id]"""
+        # try to set
+        unit_id_set = arg.strip().lower()
+        if unit_id_set:
+            try:
+                mbus_cli.unit_id = int(unit_id_set)
+            except ValueError:
+                print('unable to set unit-id')
+        # show status
+        print(f'unit-id is set to {mbus_cli.unit_id}')
+
+    def do_read_coils(self, arg: str = ''):
+        """Modbus function 1 (read coils)\n\nread_coils [address] [number of coils]"""
+        try:
+            self._parse_read_args(arg)
+            self.request.read_list = mbus_cli.read_coils(self.request.address, self.request.number)
+            self._dump_results()
+        except ValueError as e:
+            print(e)
+
+    def do_read_discrete_inputs(self, arg: str = ''):
+        """Modbus function 2 (read discrete inputs)\n\nread_discrete_inputs [address] [number of inputs]"""
+        try:
+            self._parse_read_args(arg)
+            self.request.read_list = mbus_cli.read_discrete_inputs(self.request.address, self.request.number)
+            self._dump_results()
+        except ValueError as e:
+            print(e)
+
+    def do_version(self, _arg):
+        """Print version"""
+        print(f'{NAME} {VERSION}')
+
+    def do_exit(self, _arg):
+        """Exit from cli"""
+        return True
 
 
 if __name__ == '__main__':
     # init
     mbt_cli = MbtCli()
     # parse command line args
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('--help', action='help', help='show this help message and exit')
     parser.add_argument('-d', '--debug', action='store_true', help='debug mode')
-    parser.add_argument('-H', '--host', type=str, default='localhost', help='server host (default: localhost)')
+    parser.add_argument('-h', '--host', type=str, default='localhost', help='server host (default: localhost)')
     parser.add_argument('-p', '--port', type=int, default=502, help='server TCP port (default: 502)')
     parser.add_argument('-t', '--timeout', type=float, default=5.0, help='server timeout delay in s (default: 5.0)')
     parser.add_argument('-u', '--unit-id', type=int, default=1, help='unit-id (default is 1)')
